97ac58405b25216d60e3367949e7b466678372b2
George Soulantikas <soulgeo01@gmail.com>
2026-05-16 14:44:59 +0300

    Rearrange to make more readable

--- cmd/console-wars/main.go
+++ cmd/console-wars/main.go
@@ -15,43 +15,6 @@ import (
 	"github.com/soulgeo/console-wars/internal/messages"
 )
 
-func readFromServer(conn net.Conn, inReader *bufio.Reader) {
-	connScanner := bufio.NewScanner(conn)
-	for connScanner.Scan() {
-		text := connScanner.Text()
-		if text != game.AwaitingInput {
-			fmt.Printf("%s\n", text)
-			continue
-		}
-		writeToServer(conn, inReader)
-	}
-}
-
-func writeToServer(conn net.Conn, inReader *bufio.Reader) {
-	connWriter := bufio.NewWriter(conn)
-
-	for {
-		fmt.Printf(messages.AwaitAction)
-		input, err := inReader.ReadString('\n')
-		if err != nil {
-			fmt.Printf("Input error: %v\n", err)
-			os.Exit(0)
-		}
-		input = strings.TrimSpace(input)
-		err = filterInput(input)
-		if err != nil {
-			fmt.Printf("Invalid input, try again.\n")
-			continue
-		}
-		_, err = connWriter.WriteString(input + "\n")
-		if err != nil {
-			log.Fatalf("error: %s\n", err.Error())
-		}
-		connWriter.Flush()
-		break
-	}
-}
-
 func filterInput(input string) error {
 	if input != game.Attack && input != game.Defend && input != game.Charge &&
 		input != game.Dodge &&
@@ -62,7 +25,11 @@ func filterInput(input string) error {
 }
 
 func main() {
-	serverAddr := flag.String("server", "localhost"+config.Port, "address of the game server")
+	serverAddr := flag.String(
+		"server",
+		"localhost"+config.Port,
+		"address of the game server",
+	)
 	flag.Parse()
 
 	inReader := bufio.NewReader(os.Stdin)
@@ -83,5 +50,37 @@ func main() {
 		log.Fatalf("error sending name to server: %s\n", err.Error())
 	}
 
-	readFromServer(conn, inReader)
+	connScanner := bufio.NewScanner(conn)
+	for connScanner.Scan() {
+		text := connScanner.Text()
+		if text != game.AwaitingInput {
+			// Print server messages
+			fmt.Printf("%s\n", text)
+			continue
+		}
+
+		// The server is waiting for input
+		connWriter := bufio.NewWriter(conn)
+		for {
+			fmt.Printf(messages.AwaitAction)
+			input, err := inReader.ReadString('\n')
+			if err != nil {
+				// EOF or other error, stop trying to read
+				fmt.Printf("Input error: %v\n", err)
+				os.Exit(0)
+			}
+			input = strings.TrimSpace(input)
+			err = filterInput(input)
+			if err != nil {
+				fmt.Printf("Invalid input, try again.\n")
+				continue
+			}
+			_, err = connWriter.WriteString(input + "\n")
+			if err != nil {
+				log.Fatalf("error: %s\n", err.Error())
+			}
+			connWriter.Flush()
+			break
+		}
+	}
 }
